3275c98f8fea2b19be03e0d0a7c5b07daa38aa68
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-02-06 23:13:40 +0600

    updated the main js file

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
 let start = document.querySelector('#start');
-
-start.addEventListener('click', function () {
-  let i = 0;
-
-  setInterval(function timer() {
-    console.log(++i);
-  }, 1000);
-
-  this.removeEventListener('click', timer());
-});
