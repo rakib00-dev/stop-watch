9d77e027b97a25b3cdeb66f0917082bc29fc4cd2
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-02-07 00:41:03 +0600

    added the box and styled the aria

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-let start = document.querySelector('#start');
+let stopWatch = document.getElementById('stopWatch');
+const btns = document.getElementById('buttons');
+const h1 = document.getElementById('h1');
+let [seconds, minutes, hours] = [0, 0, 0];
