bab58a4c7669771872b415c4cc147ad9a373c671
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-02-07 23:47:39 +0600

    finshed the stop watch and added the last stop watch stoper and rester

--- main.js
+++ main.js
@@ -13,9 +13,29 @@ function stopWatch() {
     }
   }
 
-  let h = hours < 10 ? '0' + hours : hours;
-  let m = minutes < 10 ? '0' + minutes : minutes;
-  let s = seconds < 10 ? '0' + seconds : seconds;
+  // let h = hours < 10 ? '0' + hours : hours;
+  let h;
+  if (hours < 10) {
+    h = '0' + hours;
+  } else {
+    h = hours;
+  }
+
+  // let m = minutes < 10 ? '0' + minutes : minutes;
+  let m;
+  if (minutes < 10) {
+    m = '0' + minutes;
+  } else {
+    m = minutes;
+  }
+
+  // let s = seconds < 10 ? '0' + seconds : seconds;
+  let s;
+  if (seconds < 10) {
+    s = '0' + seconds;
+  } else {
+    s = seconds;
+  }
 
   // timeHolder.innerHTML = `0${hours}:0${minutes}:0${seconds}`;
   timeHolder.innerHTML = h + ':' + m + ':' + s;
@@ -26,4 +46,14 @@ function watchStarter() {
     clearInterval(timer);
   }
   timer = setInterval(stopWatch, 1000);
+}
+
+function watchStoper() {
+  clearInterval(timer);
+}
+
+function watchReseter() {
+  clearInterval(timer);
+  [seconds, minutes, hours] = [0, 0, 0];
+  timeHolder.innerHTML = '00:00:00';
 }
