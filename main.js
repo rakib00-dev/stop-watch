85cbfebab68d563bb1c8bffd0c690037d739c046
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-02-07 23:07:39 +0600

    added stop watch starter

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 let timeHolder = document.getElementById('timeHolder');
-const btns = document.getElementById('buttons');
-const h1 = document.getElementById('h1');
 let [seconds, minutes, hours] = [0, 0, 0];
+let timer = 0;
 
 function stopWatch() {
   seconds++;
   if (seconds == 60) {
     seconds = 0;
     minutes++;
+    if (minutes == 60) {
+      minutes = 0;
+      hours++;
+    }
   }
+
+  let h = hours < 10 ? '0' + hours : hours;
+  let m = minutes < 10 ? '0' + minutes : minutes;
+  let s = seconds < 10 ? '0' + seconds : seconds;
+
+  // timeHolder.innerHTML = `0${hours}:0${minutes}:0${seconds}`;
+  timeHolder.innerHTML = h + ':' + m + ':' + s;
+}
+
+function watchStarter() {
+  if (timer != null) {
+    clearInterval(timer);
+  }
+  timer = setInterval(stopWatch, 1000);
 }
