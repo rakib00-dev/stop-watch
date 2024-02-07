8b0ab9746347f093423454250f73b9663ff443da
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-02-07 22:14:18 +0600

    updated the main js file function

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ let stopWatch = document.getElementById('stopWatch');
 const btns = document.getElementById('buttons');
 const h1 = document.getElementById('h1');
 let [seconds, minutes, hours] = [0, 0, 0];
+
+function stopWatch() {
+  seconds++;
+}
