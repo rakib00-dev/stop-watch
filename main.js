b758c12f579465f557c50b04de6d72bdf56de749
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-02-08 00:31:05 +0600

    adding a new feature in stop watch which is millisecond

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 let timeHolder = document.getElementById('timeHolder');
-let [seconds, minutes, hours] = [0, 0, 0];
+let millisecondInput = document.getElementById('millisecond');
+let [seconds, minutes, hours, millisecond] = [0, 0, 0, 0];
 let timer = 0;
 
 function stopWatch() {
-  seconds++;
-  if (seconds == 60) {
-    seconds = 0;
-    minutes++;
-    if (minutes == 60) {
-      minutes = 0;
-      hours++;
+  millisecond++;
+  if (millisecond == 1) {
+    millisecond = 0;
+    seconds++;
+    if (seconds == 60) {
+      seconds = 0;
+      minutes++;
+      if (minutes == 60) {
+        minutes = 0;
+        hours++;
+      }
     }
   }
 
@@ -37,8 +42,17 @@ function stopWatch() {
     s = seconds;
   }
 
+  // let s = seconds < 10 ? '0' + seconds : seconds;
+  let ms;
+  if (millisecond < 10) {
+    ms = '0' + millisecond;
+  } else {
+    ms = millisecond;
+  }
+
   // timeHolder.innerHTML = `0${hours}:0${minutes}:0${seconds}`;
   timeHolder.innerHTML = h + ':' + m + ':' + s;
+  millisecondInput.innerHTML = ms;
 }
 
 function watchStarter() {
